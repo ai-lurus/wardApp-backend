61278c4fe227e71dcb1717a214e7815a9258b611
Carlos Cortes <56169603+Liyokrn@users.noreply.github.com>
2026-04-16 11:09:59 -0600

    test(auth): add rate limiting tests

--- src/__tests__/helpers/setupEnv.ts
+++ src/__tests__/helpers/setupEnv.ts
@@ -2,3 +2,7 @@ import * as dotenv from "dotenv";
 import path from "path";
 
 dotenv.config({ path: path.resolve(__dirname, "../../../.env.test") });
+
+if (process.env.DATABASE_URL_TEST && !process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = process.env.DATABASE_URL_TEST;
+}
